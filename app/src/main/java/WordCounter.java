d62df62009c7831e0aa3db7df6f0ff5b9ee1f8eb
Ross Melville <rossmelville@hotmail.com>
2017-08-08 15:25:34 +0100

    Added a WordCounter class and WordCounterTest class. Initial commit.

---
+++ app/src/main/java/WordCounter.java
@@ -0,0 +1,12 @@
+/**
+ * Created by rossmelville on 08/08/2017.
+ */
+
+public class WordCounter {
+
+    String passage;
+
+    public WordCounter(String passage) {
+        this.passage = passage;
+    }
+}
---
+++ app/src/test/java/com/example/wordcounter/WordCounterTest.java
@@ -0,0 +1,20 @@
+package com.example.wordcounter;
+
+import org.junit.Before;
+
+/**
+ * Created by rossmelville on 08/08/2017.
+ */
+
+public class WordCounterTest {
+
+    String testPassage;
+
+    @Before
+    public void before(){
+        testPassage = "Hello my name is Ross";
+    }
+
+    
+
+}
